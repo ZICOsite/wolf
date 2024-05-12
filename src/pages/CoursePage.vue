d54342406747dc77f7581f615f2d4bb75f8f1d34
Sanjar <alisherovsanjarbek917@gmail.com>
2024-05-12 19:45:10 +0500

    section free tour form

--- src/pages/CoursePage.vue
+++ src/pages/CoursePage.vue
@@ -3,12 +3,23 @@ import SalesBannerView from "@/views/salesBannerView/SalesBannerView.vue";
 import ResultInfoView from "@/views/resultInfoView/ResultInfoView.vue";
 import ProblemsView from "@/views/problemsView/ProblemsView.vue";
 import ResultsView from "@/views/resultsView/ResultsView.vue";
+import FreetourView from "@/views/freetourView/FreetourView.vue";
 </script>
 <template>
   <main>
     <SalesBannerView />
     <ResultInfoView />
     <ProblemsView />
-    <ResultsView title="Посмотрите результаты компаний, которые уже прошли курс"/>
+    <ResultsView
+      title="Посмотрите результаты компаний, которые уже прошли курс"
+    />
+    <FreetourView
+      inputBg="var(--secondary-color)"
+      inputColor="var(--primary-color)"
+      sectionBg="var(--white)"
+      titleColor="var(--primary-color)"
+      btnBg="var(--primary-color)"
+      btnColor="var(--white)"
+    />
   </main>
 </template>
