55f0093eb74efd44a15df4ca83604642acbb4356
Sanjar <alisherovsanjarbek917@gmail.com>
2024-05-16 15:05:54 +0500

    clientsView completed

---
+++ src/views/clientsView/ClientsView.vue
@@ -0,0 +1,46 @@
+<script setup>
+import Typography from "@/components/UI/Typography.vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+
+const modules = [Autoplay];
+
+const breakpoints = {
+  320: {
+    slidesPerView: 2.5,
+  },
+  850: {
+    slidesPerView: 5.5,
+  },
+};
+</script>
+
+<template>
+  <section class="clients">
+    <div class="container">
+      <Typography class="clients__title title" tagName="h2"
+        >Среди наших клиентов</Typography
+      >
+      <div class="clients__carousel">
+        <Swiper
+          class="clients__carousel-slider"
+          :modules="modules"
+          :spaceBetween="16"
+          :loop="true"
+          :noSwiping="true"
+          :breakpoints="breakpoints"
+          :autoplay="{
+            delay: 0,
+          }"
+        >
+          <SwiperSlide v-for="item in 20" :key="item">
+            <img src="@i/logo.svg" alt="" class="clients__carousel-image" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  </section>
+</template>
