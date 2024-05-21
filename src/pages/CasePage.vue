b1ee3d989ada3aa0188cfbd2c47b8c1aab6ca5f3
Sanjar <alisherovsanjarbek917@gmail.com>
2024-05-21 17:04:01 +0500

    rightbox and revenue sections finished

--- src/pages/CasePage.vue
+++ src/pages/CasePage.vue
@@ -1,12 +1,16 @@
 <script setup>
+import Typography from '@/components/UI/Typography.vue';
 import { useRoute } from "vue-router";
-
+import RightBoxView from '@/views/rightBoxView/RightBoxView.vue'
+import RevenueView from '@/views/revenueView/RevenueView.vue'
 const route = useRoute()
 
 </script>
 
 <template>
     <main>
-        <h1>This is {{ route.params.id }}</h1>
+        <!-- <h1>This is {{ route.params.id }}</h1> -->
+        <RightBoxView/>
+        <RevenueView/>
     </main>
 </template>
---
+++ src/views/rightBoxView/RightBoxView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Typography from "@/components/UI/Typography.vue";
+import Button from "@/components/UI/Button.vue";
+</script>
+<template>
+    <section class="rightbox">
+        <div class="container">
+            <div class="rightbox__descr">
+                <Typography tagName="h2" class="title rightbox__title">
+                    Райтбокс
+                </Typography>
+                <Typography tagName="p" class="rightbox__txt">
+                    Изготовление упаковки из тонкого картона
+                </Typography>
+                <Button bg="#24353C" color="var(--white)" class="rightbox__btn">Сайт компании</Button>
+            </div>
+            <img src="@i/rightbox.jpg" alt="" class="rightbox__img">
+        </div>
+    </section>
+</template>
+
