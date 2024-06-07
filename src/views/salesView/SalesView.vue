ace3fcf659eaf42db7dedea52989d8d9937b9526
Sanjar <alisherovsanjarbek917@gmail.com>
2024-06-07 20:57:45 +0500

    slider api and statistics

--- src/views/salesView/SalesView.vue
+++ src/views/salesView/SalesView.vue
@@ -9,12 +9,16 @@ import { IconArrow } from "@/components/icons/icons";
 import Modal from "@/components/UI/Modal.vue";
 import Form from "@/components/UI/Form.vue";
 import Input from "@/components/UI/Input.vue";
+import useFetch from "@/services/api";
 const modules = ref([Navigation, Autoplay, Pagination]);
 const navigationButtons = {
   nextEl: ".sales__swiper-next",
   prevEl: ".sales__swiper-prev",
 };
 const modal = ref(false)
+const { data, isFetching, fetchData } = useFetch();
+
+fetchData("common/main-page/");
 </script>
 
 <template>
@@ -22,20 +26,20 @@ const modal = ref(false)
     <div class="container">
       <swiper :navigation="navigationButtons" :modules="modules" :autoplay="true" class="mySwiper sales__swiper"
         :loop="true" :pagination="true" :spaceBetween="10">
-        <swiper-slide class="sales__swiper-slide" v-for="item in 3" :key="item">
+        <swiper-slide class="sales__swiper-slide" v-for="item in data" :key="item">
           <div class="sales__swiper-item">
             <div class="sales__swiper-content">
               <Typography tagName="h2" class="sales__swiper-title title">
-                Построение отделов продаж под ключ за 4 месяца
+                {{item.title}}
               </Typography>
               <Typography tagName="p" class="sales__swiper-txt">
-                Запишитесь на бесплтную онлайн-экскурсию в наш отдел продаж и
-                получите новый взгляд, каким может быть крутой отдел продаж.
+               {{item.description}}
               </Typography>
-              <Button @click="modal = true" bg="var(--primary-color)" color="var(--white)" class="sales__swiper-btn">Записаться на онлайн
+              <Button @click="modal = true" bg="var(--primary-color)" color="var(--white)"
+                class="sales__swiper-btn">Записаться на онлайн
                 экскурсию</Button>
             </div>
-            <img src="@i/slider-sales.jpg" alt="" class="sales__swiper-img" />
+            <img :src="item.image" alt="" class="sales__swiper-img" />
           </div>
         </swiper-slide>
         <div class="sales__swiper-buttons">
@@ -54,10 +58,10 @@ const modal = ref(false)
           <Typography tagName="h2" class="form__title">Закажите отдел продаж</Typography>
           <Typography tagName="p" class="form__txt">Постройте систему в отделе продаж и увеличите оборот компании в два
             раза</Typography>
-            <Input placeholder="Имя"/>
-            <Input placeholder="Номер телефона"/>
-            <Input placeholder="Электронная почта"/>
-            <Button bg="var(--white)" color="var(--primary-color)">Записаться на экскурсию</Button>
+          <Input placeholder="Имя" />
+          <Input placeholder="Номер телефона" />
+          <Input placeholder="Электронная почта" />
+          <Button bg="var(--white)" color="var(--primary-color)">Записаться на экскурсию</Button>
         </Form>
       </Modal>
     </Teleport>
