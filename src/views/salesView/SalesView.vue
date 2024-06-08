4f2657f57cc90f59121a1f93c20f2593bea15195
Sanjar <alisherovsanjarbek917@gmail.com>
2024-06-08 18:07:56 +0500

    skeleton api

--- src/views/salesView/SalesView.vue
+++ src/views/salesView/SalesView.vue
@@ -10,6 +10,7 @@ import Modal from "@/components/UI/Modal.vue";
 import Form from "@/components/UI/Form.vue";
 import Input from "@/components/UI/Input.vue";
 import useFetch from "@/services/api";
+import Skeleton from "@/components/UI/Skeleton.vue";
 const modules = ref([Navigation, Autoplay, Pagination]);
 const navigationButtons = {
   nextEl: ".sales__swiper-next",
@@ -24,33 +25,37 @@ fetchData("common/main-page/");
 <template>
   <section class="sales">
     <div class="container">
-      <swiper :navigation="navigationButtons" :modules="modules" :autoplay="true" class="mySwiper sales__swiper"
-        :loop="true" :pagination="true" :spaceBetween="10">
-        <swiper-slide class="sales__swiper-slide" v-for="item in data" :key="item">
-          <div class="sales__swiper-item">
-            <div class="sales__swiper-content">
-              <Typography tagName="h2" class="sales__swiper-title title">
-                {{item.title}}
-              </Typography>
-              <Typography tagName="p" class="sales__swiper-txt">
-               {{item.description}}
-              </Typography>
-              <Button @click="modal = true" bg="var(--primary-color)" color="var(--white)"
-                class="sales__swiper-btn">Записаться на онлайн
-                экскурсию</Button>
+      <Skeleton :loader="isFetching" :count="1">
+        <template #default>
+        <swiper :navigation="navigationButtons" :modules="modules" :autoplay="true" class="mySwiper sales__swiper"
+          :loop="true" :pagination="true" :spaceBetween="10">
+            <swiper-slide class="sales__swiper-slide" v-for="item in data" :key="item">
+              <div class="sales__swiper-item">
+                <div class="sales__swiper-content">
+                  <Typography tagName="h2" class="sales__swiper-title title">
+                    {{ item.title }}
+                  </Typography>
+                  <Typography tagName="p" class="sales__swiper-txt">
+                    {{ item.description }}
+                  </Typography>
+                  <Button @click="modal = true" bg="var(--primary-color)" color="var(--white)"
+                    class="sales__swiper-btn">Записаться на онлайн
+                    экскурсию</Button>
+                </div>
+                <img :src="item.image" alt="" class="sales__swiper-img" />
+              </div>
+            </swiper-slide>
+            <div class="sales__swiper-buttons">
+              <button class="sales__swiper-prev">
+                <IconArrow direction="left" color="var(--white)" />
+              </button>
+              <button class="sales__swiper-next">
+                <IconArrow direction="right" color="var(--white)" />
+              </button>
             </div>
-            <img :src="item.image" alt="" class="sales__swiper-img" />
-          </div>
-        </swiper-slide>
-        <div class="sales__swiper-buttons">
-          <button class="sales__swiper-prev">
-            <IconArrow direction="left" color="var(--white)" />
-          </button>
-          <button class="sales__swiper-next">
-            <IconArrow direction="right" color="var(--white)" />
-          </button>
-        </div>
-      </swiper>
+          </swiper>
+        </template>
+      </Skeleton>
     </div>
     <Teleport to="#body" v-if="modal">
       <Modal @click="modal = false">
